cd8bcf201acf583b0d10b8e6e35537e396390005
Isaac3N <theisaac3n@gmail.com>
2021-05-29 16:19:43 +0100

    updated views

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -11,17 +11,17 @@ class BlogPostListView(ListAPIView):
     lookup_field = 'slug'
     permission_classes = (permissions.AllowAny, )
 
-# class BlogPostDetailView(RetrieveAPIView):
-#     queryset = BlogPost.objects.order_by('-date_created')
-#     serializer_class = BlogPostSerializer
-#     lookup_field = 'slug'
-#     permission_classes = (permissions.AllowAny, )
+class BlogPostDetailView(RetrieveAPIView):
+    queryset = BlogPost.objects.order_by('-date_created')
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny, )
 
-# class BlogPostFeaturedView(ListAPIView):
-#     queryset = BlogPost.objects.all().filter(featured=True)
-#     serializer_class = BlogPostSerializer
-#     lookup_field = 'slug'
-#     permission_classes = (permissions.AllowAny, )
+class BlogPostFeaturedView(ListAPIView):
+    queryset = BlogPost.objects.all().filter(featured=True)
+    serializer_class = BlogPostSerializer
+    lookup_field = 'slug'
+    permission_classes = (permissions.AllowAny, )
 
 # class BlogPostCategoryView(APIView):
 #     serializer_class = BlogPostSerializer
